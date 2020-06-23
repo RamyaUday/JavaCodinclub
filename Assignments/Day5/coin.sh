896d4f047288db55a559610b1ee944459fdb8d22
RamyaaU <43752215+RamyaaU@users.noreply.github.com>
2020-06-23 16:16:30 +0530

    Add files via upload

---
+++ Assignments/Day5/coin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+HEAD=0
+TAIL=0
+
+for x in 1
+do
+
+FLIP=$(($(($RANDOM%10))%2))
+
+if [ $FLIP -eq 1 ]
+then
+echo "TAIL"
+TAIL=$(($TAIL+1))
+else
+echo "HEAD"
+HEAD=$(($HEAD+1))
+fi
+done
